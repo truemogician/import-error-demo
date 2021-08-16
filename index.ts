1a2a4661e1aa2a56f4970c35fa4ad9bdefb1ddbc
true_mogician <925269713@qq.com>
2021-08-16 17:58:04 +0800

    add comment

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 import foo from "foo";
 import bar from "./localbar";
+//Error: Cannot find module.
+//But if we compile ./localbar.ts and place the result in node_modules with a package.json
+//that configures it as a CommonJS module, then import from node_modules like
+//`import bar from "bar"`, everything goes fine.
 foo();
 bar();
